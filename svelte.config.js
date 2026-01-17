23f615e139eae33ab95a0726ffea2e5424e221a4
𝖒𝖎𝖍𝖓𝖊𝖆 𝖒𝖔𝖘𝖔 <sons.mosorescu@gmail.com>
2026-01-17 22:49:23 +0200

    added cloudflare pages deployment support

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// Use Cloudflare adapter when CF_PAGES is set (Cloudflare Pages build environment)
+const adapter = process.env.CF_PAGES ? adapterCloudflare() : adapterAuto();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +15,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter
 	}
 };
 
